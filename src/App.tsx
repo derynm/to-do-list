bdcce0e84ea34141407140bdfba97105592d57c0
derynm <dery.marjuki@gli.id>
2024-05-13 13:46:26 +0700

    feat: Set up project structure and initial files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <p className="bg-red-500 text-2xl">HEllo World</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,7 @@
+export default function NavBar() {
+  return (
+    <div className="bg-blue-500 text-white">
+      <p>To Do List</p>
+    </div>
+  );
+}
