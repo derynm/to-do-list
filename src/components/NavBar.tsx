3ba1593499537101f2aab38729fbd36e82edbe01
derynm <dery.marjuki@gli.id>
2024-06-05 16:59:04 +0700

    fix: change a tag to navLink

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,18 @@
 import { AiOutlineHistory } from 'react-icons/ai'
+import { NavLink } from 'react-router-dom'
 
 export function NavBar() {
   return (
     <nav className="flex items-center gap-5 bg-slate-500 p-4 text-white">
-      <a href="/" className="text-2xl font-semibold">
+      <NavLink to="/" className="text-2xl font-semibold">
         To Do List
-      </a>
-      <a
-        href="/activity"
+      </NavLink>
+      <NavLink
+        to="/activity"
         className="flex items-center gap-2 rounded-lg p-2 text-xl hover:bg-slate-800">
         <AiOutlineHistory />
         Activity
-      </a>
+      </NavLink>
     </nav>
   )
 }
